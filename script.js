ff2305bfcc70b06dfe714d50c38766382c4b818e
mishkatjamal <mishkatjamal13@gmail.com>
2025-11-22 00:29:38 +0530

    piano making dom

--- script.js
+++ script.js
@@ -1,75 +1,52 @@
-const arr = [
-    {
-        fullName: "Aarav Mehta",
-        image: "https://plus.unsplash.com/premium_photo-1763369800370-a89b6fb2a3d2?q=80&w=774&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        profession: "UX Designer",
-        description: "Designs apps that feel human. Loves coffee & pixel-perfect layouts.",
-        background:"https://images.unsplash.com/photo-1674707488760-4ec87e969368?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8M2QlMjBiYWNrZ3JvdW5kfGVufDB8fDB8fHww",
-        tags: ["Figma", "UI/UX", "Design Systems", "Freelancer"]
-    },
-    {
-        fullName: "Emily Carter",
-        image: "https://images.unsplash.com/photo-1552374196-c4e7ffc6e126",
-        profession: "Software Engineer",
-        description: "Writes clean code and breaks things just to fix them better.",
-        background:"https://images.unsplash.com/photo-1724405143873-cdaa5cac918e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8M2QlMjBiYWNrZ3JvdW5kfGVufDB8fDB8fHww",
-        tags: ["React", "TypeScript", "Open Source", "Tech Speaker"]
-    },
-    {
-        fullName: "Aisha Khan",
-        image: "https://images.unsplash.com/photo-1517841905240-472988babdf9",
-        profession: "Data Scientist",
-        description: "Finds patterns in the noise. Speaks fluently in charts and numbers.",
-        background:"https://images.unsplash.com/photo-1662869633285-13fe05236a72?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fDNkJTIwYmFja2dyb3VuZHxlbnwwfHwwfHx8MA%3D%3D",
-        tags: ["Python", "AI", "Machine Learning", "Analytics"]
-    },
-    {
-        fullName: "Noah Williams",
-        image: "https://images.unsplash.com/photo-1494790108377-be9c29b29330",
-        profession: "Content Creator",
-        description: "Makes complex topics easy. Lives on caffeine and camera angles.",
-        background:"https://plus.unsplash.com/premium_photo-1672312039942-edce7c8eb764?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8M2QlMjBiYWNrZ3JvdW5kfGVufDB8fDB8fHww",
-        tags: ["Video Editing", "Education", "YouTube", "Storytelling"]
-    }
+let keys = document.querySelectorAll(".key");
+
+const sound = [
+    "piano/28.mp3",
+    "piano/29.mp3",
+    "piano/30.mp3",
+    "piano/31.mp3",
+    "piano/32.mp3",
+    "piano/33.mp3",
+    "piano/34.mp3",
+    "piano/35.mp3",
+    "piano/36.mp3",
+    "piano/37.mp3",
+    "piano/38.mp3",
+    "piano/39.mp3",
+    "piano/40.mp3",
+    "piano/41.mp3",
+    "piano/42.mp3",
+    "piano/43.mp3",
+    "piano/44.mp3",
+    "piano/45.mp3",
+    "piano/46.mp3",
+    "piano/47.mp3",
+    "piano/48.mp3",
+    "piano/49.mp3",
+    "piano/50.mp3",
+    "piano/51.mp3",
+    "piano/52.mp3",
+    "piano/53.mp3",
+    "piano/54.mp3",
+    "piano/55.mp3",
+    "piano/56.mp3",
+    "piano/57.mp3",
+    "piano/58.mp3"
 ];
 
+keys.forEach((val,ind)=>{
+    val.addEventListener("click",()=>{
+        let audio = new Audio(sound[ind]);
+        audio.play();
+    });
+});
 
-
-let sum = "";
-
-arr.forEach((val)=>{
-    let randomTag = val.tags[Math.floor(Math.random() * val.tags.length)];
-
-    sum += `   <div id="card">
-            <img id="bg-img" src="${val.background}" alt="">
-
-            <div id="profile">
-                <img src="${val.image}" alt="">
-                <div id="profile-inner">
-                    <i class="ri-diamond-fill"></i>
-                    <h5>${randomTag}</h5>
-                </div>
-            </div>
-
-          <div id="text">
-
-             <div id="text-inner">
-                 <h3>${val.fullName}</h3>
-            <h5>${val.profession}</h5>
-             </div>
-
-            <div id="text-icon">
-                <i class="ri-book-marked-fill"></i>
-            </div>
-            
-          </div>
-
-          <p>${val.description}</p>
-          <button>Get in touch</button>
-        </div>`  
-        
+// key press wala hai ye 
+document.addEventListener("keydown",(dets)=>{
+    keys.forEach((key,indx)=>{
+        if (key.innerText.toLowerCase() === dets.key.toLocaleLowerCase()) {
+            new Audio(sound[indx]).play()
+        }
+    })
+    
 })
-
-let main = document.querySelector("main")
-
-main.innerHTML =sum
