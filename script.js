8689bf3eea6961645b7011429785f70b31edfcaa
mishkatjamal <mishkatjamal13@gmail.com>
2025-11-27 11:02:36 +0530

    random text change

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
-addEventListener("mousemove",(e)=>{
-    console.log(e.clientX,e.clientY);
-    document.body.style.setProperty("--x",e.clientX+"px")
-    document.body.style.setProperty("--y",e.clientY+"px")
-    
+const cha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+const para = document.querySelector("p")
+const text = para.innerText;
+
+para.addEventListener("mouseenter",()=>{
+
+    let stop = setInterval(()=>{
+    const str =text.split("").map((char,ind)=>{
+        return cha.split("")[Math.floor(Math.random()*53)]
+    }).join("")
+    para.innerText = str
+},30)
+
+setTimeout(()=>{
+clearInterval(stop);
+para.innerText = text;
+},2000)
 })
